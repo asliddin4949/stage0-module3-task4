fed681cf7f6c5b226819ebbeb4e1498a7e8a14de
asliddin4949 <asliddin4949@inbox.ru>
2022-11-30 18:52:38 +0500

    task done

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class FormulaCreator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a,b,formula;
+        double a,b,formula;
         System.out.print("a = ");
         a=scanner.nextInt();
         System.out.print("b = ");
         b=scanner.nextInt();
-        formula = (9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
+        formula =( (9 * a * a - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) / 2);
         System.out.println("formula = " +formula);
+      //  System.out.println("(9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2)");
     }
 }
